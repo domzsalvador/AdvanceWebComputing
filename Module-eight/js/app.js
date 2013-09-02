958c808001e1d15fad4c60a6b78fe1829e64d5ed
leelopez <leechristianlopez@gmail.com>
2013-09-02 14:11:51 -0800

    pre function, unfinished layout

--- Module-eight/js/app.js
+++ Module-eight/js/app.js
@@ -1,27 +1,29 @@
 $(function () {
 	
 	var url = 'http://api.rottentomatoes.com/api/public/v1.0/';
+	var api_key = 'gvru729uj8x33ykh4zjvw4nv';
+	var data = {apiKey:api_key};
 	var keyword;
-
-	function request(url, data, callback){
+	
+	function request(urlIn, dataIn){
 		$.ajax({
-			url: url,
+			url: urlIn,
 			dataType: 'jsonp',
-			data: data,
-			success: callback
+			data: dataIn,
+			success: showMovies
 		});
 
 	}
 	
-	function getTemplate(template_name, data){
+	function getTemplate(template_name, dataIn){
 		var markup = '';
 		var template = $('#' + template_name).html();
 		var $template = Handlebars.compile(template);
-		markup = $template(data);
+		markup = $template(dataIn);
 		return markup;
 	}
 
-	function ShowMovies(response){
+	function showMovies(response){
 		console.log(response);
 		var myNode = document.getElementById("list");
 		myNode.innerHTML = '';
@@ -30,7 +32,6 @@ $(function () {
 		for (var i = 0; i < movies.length; i++) {
 			var movie = response.movies[i];
 			$('ul').append(getTemplate('tpl-movie', movie));
-			//$('thumbnails').append(getTemplate('tpl-movie', movie));
 		}
 	}
 	
@@ -42,23 +43,40 @@ $(function () {
 	
 	$('#btn-search').click(function(){
 		keyword = $('#textbox').val();
-		request(url + "movies.json", {q:keyword, apiKey:'gvru729uj8x33ykh4zjvw4nv'}, ShowMovies);
+		var search_data = {q:keyword, apiKey:api_key};
+		request(url + "movies.json", search_data);
 	});
 	
 	$('#box-office').click(function(){
-		request(url + "lists/movies/box_office.json", {apiKey:'gvru729uj8x33ykh4zjvw4nv'}, ShowMovies);
+		request(url + "lists/movies/box_office.json", data);
 	});
 	
 	$('#in-theaters').click(function(){
-		request(url + "lists/movies/in_theaters.json", {apiKey:'gvru729uj8x33ykh4zjvw4nv'}, ShowMovies);
+		request(url + "lists/movies/in_theaters.json", data);
 	});
 	
-	$('#opening').click(function(){
-		request(url + "lists/movies/opening.json", {apiKey:'gvru729uj8x33ykh4zjvw4nv'}, ShowMovies);
+	$('#opening-movies').click(function(){
+		request(url + "lists/movies/opening.json", data);
 	});
 	
-	$('#upcoming').click(function(){
-		request(url + "lists/movies/upcoming.json", {apiKey:'gvru729uj8x33ykh4zjvw4nv'}, ShowMovies);
+	$('#upcoming-movies').click(function(){
+		request(url + "lists/movies/upcoming.json", data);
+	});
+	
+	$('#top-rentals').click(function(){
+		request(url + "lists/dvds/top_rentals.json", data);
+	});
+	
+	$('#current-release').click(function(){
+		request(url + "lists/dvds/current_releases.json", data);
+	});
+	
+	$('#new-release-dvds').click(function(){
+		request(url + "lists/dvds/new_releases.json", data);
+	});
+	
+	$('#upcoming-dvds').click(function(){
+		request(url + "lists/dvds/upcoming.json", data);
 	});
 	
 });
